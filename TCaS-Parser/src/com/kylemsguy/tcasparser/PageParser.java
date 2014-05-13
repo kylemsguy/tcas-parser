1f87525003470995bf4fef83d3f61c2e10c2cfca
Kyle Zhou <kylezhou2002@hotmail.com>
2014-05-12 17:46:23 -0700

    First commit.

---
+++ TCaS-Parser/src/com/kylemsguy/tcasparser/PageParser.java
@@ -0,0 +1,15 @@
+package com.kylemsguy.tcasparser;
+
+import java.net.URL;
+
+public class PageParser {
+
+	public PageParser(){
+		// pass
+	}
+	
+	public String getPage(String urlString){
+		
+	}
+
+}
