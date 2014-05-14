7182a930f8da156485dd6906f842bc3305297158
Kyle <kylezhou2002@hotmail.com>
2014-05-13 21:44:28 -0700

    Starting to implement question asking

---
+++ TCaS-Parser/src/com/kylemsguy/tcasparser/QuestionManager.java
@@ -0,0 +1,19 @@
+package com.kylemsguy.tcasparser;
+
+public class QuestionManager {
+	
+	private SessionManager session;
+
+	public QuestionManager(SessionManager session) {
+		this.session = session;
+	}
+	
+	public void ask_question(String question){
+		// do stuff
+	}
+	
+	public void get_questions(){
+		// TODO implement getting questions
+	}
+
+}
