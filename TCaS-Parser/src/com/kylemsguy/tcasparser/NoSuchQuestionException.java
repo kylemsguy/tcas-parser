1c303090a40c7e4e84ece5e4e28aabba22f5b453
Kyle Zhou <kylezhou2002@hotmail.com>
2014-05-22 13:56:18 -0700

    Now compatible as Android project

---
+++ TCaS-Parser/src/com/kylemsguy/tcasparser/NoSuchQuestionException.java
@@ -0,0 +1,30 @@
+package com.kylemsguy.tcasparser;
+
+public class NoSuchQuestionException extends Exception {
+
+	public NoSuchQuestionException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoSuchQuestionException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoSuchQuestionException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoSuchQuestionException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoSuchQuestionException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
